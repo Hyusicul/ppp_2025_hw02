7f077d7c46ff05201af64700d10bb38a97a0d683
Park Hyeon <105685800+Hyusicul@users.noreply.github.com>
2025-03-13 20:09:29 +0900

    Add files via upload

---
+++ hw02_circle.py
@@ -0,0 +1,4 @@
+r=4
+pi=3.14
+circle_area=r*r*pi
+print("반지름이{}인 원의 면적은 약{}입니다.".format(r,circle_area))
---
+++ hw02_sale.py
@@ -0,0 +1,4 @@
+regular_price=2000
+sale_per=15
+sale_price=regular_price-(regular_price*(sale_per/100))
+print("정가가 {}인 물건에 {}%의 할인율을 적용하면 최종가격은 {}입니다.".format(regular_price, sale_per, sale_price))
